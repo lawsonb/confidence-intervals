13b614fce7f2772d36a00f19bf5c447e947fba06
Brian <lawsonb@users.noreply.github.com>
2015-03-01 23:51:49 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,21 +9,25 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
+      actionButton("action", label="Refresh"),
       sliderInput("cr",
                   "Coverage rate:",min = 0.500,max = 0.999,value = 0.95),
       sliderInput("trials",
                   "Number of samples:",min = 20,max = 200,value = 100),
       sliderInput("size",
                   "Size of sample:",min = 10,max = 1000,value = 50),
-      sliderInput("mean",
-                  "Mean:",min = -10,max = 10,value = 0),
       sliderInput("sd",
                   "Standard Deviation:",min = 0.1,max = 10.0,value = 2),
-      actionButton("action", label="Refresh")
-    ),
+      sliderInput("mean",
+                  "Mean:",min = -10,max = 10,value = 0)
+     ),
     # Show a plot of the trials
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot"),
+      br(),
+      p("Code ",
+        a("here", 
+          href = "https://github.com/lawsonb/confidence-intervals"))
     )
   )
 ))
