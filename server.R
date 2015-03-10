96fac830c51a155acee85ed71c05ce662b2ad6da
Brian <lawsonb@users.noreply.github.com>
2015-03-09 21:54:31 -0700

    Update server.R

--- server.R
+++ server.R
@@ -16,10 +16,10 @@ shinyServer(function(input, output) {
   mean.unknown = function(cr, n) qnorm( (cr + 1)/2 ) / sqrt(n)
   
   # var.unknown
- 
+  
   # blue if interval contains the parameter, red if it does not
   infocol = c("blue", "red")
-      
+  
   make.plots = function(type.unknown = "Mean and Variance") {
     cr = input$cr
     t = input$trials
@@ -31,13 +31,14 @@ shinyServer(function(input, output) {
     # the value c.sqrtn is the same for every coverage rate cr and n
     if (type.unknown == "Mean and Variance") c.sqrtn = both.unknown(cr, n) 
     if (type.unknown == "Only Mean") c.sqrtn = mean.unknown(cr, n) 
-
+    xrangelen = c(0,4*sd*c.sqrtn)
+    
     # calculate interval for t trials and coverage
     interval = sapply(1:t, function(k) {
       sample = rnorm(n, m, sd)
       sample.mean = mean(sample)
       if (type.unknown == "Mean and Variance") 
-          half.length = c.sqrtn * sd(sample) else {
+        half.length = c.sqrtn * sd(sample) else {
           half.length = c.sqrtn * sd 
         }
       lower = sample.mean - half.length
@@ -45,7 +46,7 @@ shinyServer(function(input, output) {
       if (lower <= m &  m <= upper ) cover = 1 else cover = 2
       c(lower, upper, 2*half.length, cover)
     })
-     
+    
     # draw first trial separately to set up the plot
     par(mfrow=c(1,2))
     plot(interval[1:2,1], c(1,1), type = "l", col = infocol[ interval[4,1] ],
@@ -59,20 +60,26 @@ shinyServer(function(input, output) {
     
     # this plots the remaining 2:t trials
     for (k in 2:t) lines( interval[1:2, k], c(k, k), 
-                         col = infocol[ interval[4,k] ] )
+                          col = infocol[ interval[4,k] ] )
     
     # this does the second plot -- density plot of interval lengths
-    plot(density(interval[3,]), main="Interval Lengths")
-    if (type.unknown == "Only Mean") 
+    if (type.unknown == "Only Mean") {
+      plot(0,0, main="Interval Lengths", type = "n",
+           xlim = xrangelen, ylim = c(0,1),
+           xlab = "interval length")
+      abline( v = interval[3,1] )
       mtext(paste("All intervals length",
-                  round(mean(interval[3,]),3)))  #,sd(interval[3,])))
-    else {
+                  round(mean(interval[3,]),3)))  
+    }
+     else {
+      plot(density(interval[3,]), main="Interval Lengths",
+           xlim = xrangelen, xlab = "interval length")
       mtext(paste0("Interval mean=",round(mean(interval[3,]),3),
-      " sd=",round(sd(interval[3,]),3)))
+                   " sd=",round(sd(interval[3,]),3)))
     }
   }
   
   output$unknown.both.Plot <- renderPlot( make.plots("Mean and Variance") )
   output$unknown.mean.Plot <- renderPlot( make.plots("Only Mean") )
-   
+  
 })
